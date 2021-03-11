4906e489940debecfda179a60d1cd9927ccebba2
teuteuf <antoine.teuf@gmail.com>
2021-03-11 22:52:53 +0100

    improve leaderboard

--- twitch-bot-motsfleches-app/src/components/Leaderboard.tsx
+++ twitch-bot-motsfleches-app/src/components/Leaderboard.tsx
@@ -1,18 +1,21 @@
+import classes from './Leaderboard.module.css'
+
 interface LeaderboardProps {
     leaderboard: Record<string, number>
 }
 
-function Leaderboard ({leaderboard}: LeaderboardProps) {
+function Leaderboard({leaderboard}: LeaderboardProps) {
     return (
-        <ul>
+        <ol className={classes.leaderboard}>
             {Object.entries(leaderboard)
                 .sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
-                .map(([key, value]) => (
-                    <li key={key}>
-                        {key} - {value}
+                .map(([pseudo, points]) => (
+                    <li key={pseudo}>
+                        <div className={classes.pseudo}>{pseudo}</div>
+                        <div className={classes.points}>{points} {points > 1 ? 'points' : 'point'}</div>
                     </li>
                 ))}
-        </ul>
+        </ol>
     )
 }
 
